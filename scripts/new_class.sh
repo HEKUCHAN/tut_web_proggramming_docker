5e153f3fe6f66a41b9a03f673db7b8b16b9f325e
HEKUCHAN <heitorhirose@gmail.com>
2024-11-15 00:00:28 +0900

    Add: Add docker files with apache configured cgi

---
+++ scripts/new_class.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+use_template=false
+template_folder="template"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --template)
+      use_template=true
+      shift
+      ;;
+    --with-no-html-folder)
+      exclude_html=true
+      shift
+      ;;
+    *)
+      echo "Usage: $0 [--template] [--with-no-html-folder]"
+      exit 1
+      ;;
+  esac
+done
+
+if $use_template && [ ! -d "$template_folder" ]; then
+  echo "Error: Template folder '$template_folder' does not exist."
+  exit 1
+fi
+
+last_number=0
+
+for folder in class-*; do
+  if [[ -d "$folder" && "$folder" =~ ^class-([0-9]+)$ ]]; then
+    number=${BASH_REMATCH[1]}
+    if (( number > last_number )); then
+      last_number=$number
+    fi
+  fi
+done
+
+next_number=$(printf "%02d" $((last_number + 1)))
+new_folder="class-$next_number"
+
+if $use_template; then
+  if $exclude_html; then
+    rsync -av --exclude="html" "$template_folder/" "$new_folder"
+    echo "Created folder: $new_folder (copied from $template_folder without html folder)"
+  else
+    cp -r "$template_folder" "$new_folder"
+    echo "Created folder: $new_folder (copied from $template_folder)"
+  fi
+else
+  mkdir "$new_folder"
+  echo "Created empty folder: $new_folder"
+fi
---
+++ template/html/cgi/test.cgi
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+print <<END;
+Content-type: text/html
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<title>Test CGI</title>
+</head>
+<body>
+<p>Hello World</p>
+</body>
+</html>
+END
+exit;
